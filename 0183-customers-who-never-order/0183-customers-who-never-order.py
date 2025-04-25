28514591c389edde08f31e132228549b097f3169
kishorek0344 <156921551+kishorek0344@users.noreply.github.com>
2025-04-25 19:58:19 +0530

    Time: 281 ms (79.21%), Space: 67.1 MB (63.72%) - LeetHub

---
+++ 0183-customers-who-never-order/0183-customers-who-never-order.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    
+    result = customers[~customers['id'].isin(orders['customerId'])][['name']]
+    result.columns = ['Customers']
+    result_df = pd.DataFrame(result)
+    return result_df
+    
